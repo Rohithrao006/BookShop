63ae5329860128df0c883a1586adeb31019e2345
rohith <rohithrao006@gmail.com>
2017-01-04 20:44:24 +0530

    citystate

--- Startup.cs
+++ Startup.cs
@@ -84,6 +84,10 @@ namespace WebApplication
                     template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
                 routes.MapRoute(
+                name: "areaRoute",
+                template: "{area:exists}/{controller=CityState}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
