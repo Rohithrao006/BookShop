b87d547b4f6a34b9c6fd4bf6e1fd5fdf1823563a
arunkumar.thammali <arunkumar.thammali@gmail.com>
2017-01-05 13:00:13 +0530

    merged

--- Areas/Admin/Models/BookShopDBContext.cs
+++ Areas/Admin/Models/BookShopDBContext.cs
@@ -1,41 +1,14 @@
 using Microsoft.EntityFrameworkCore;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> af3c43dd58a43589183061b72c6cce301eef254f
-
 namespace BookShop.Areas.Admin.Models
 {
     public class BookShopDBContext:DbContext
     {
-<<<<<<< HEAD
         public BookShopDBContext(DbContextOptions options):base(options)
         {
 
         }
-       // public DbSet<Customer> Customers{get;set;}
-=======
-        public BookShopDBContext()
-        {
-
-        }
+        public DbSet<Customer> Customers{get;set;}
         public DbSet<Countries> Countries{get;set;}
-=======
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApplication.Areas.Admin.Models
-{
-    public class BookShopDBContext:DbContext
-    {
-        public BookShopDBContext()
-        {
-            
-        }
         public DbSet<CityState> CitySates{get; set;}
->>>>>>> 716bee1b11c87c6e1042a51c4ffd00076d567679
->>>>>>> af3c43dd58a43589183061b72c6cce301eef254f
     }
 }
